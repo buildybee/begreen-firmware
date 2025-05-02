7599399938947f48b3645a5152522a6396f828c8
saypaul <saypaul@redhat.com>
2025-05-02 12:48:19 +0530

    when setting scehule for next day it was setting from current time
    fixed it to set the time set by user for next day

--- objects.h
+++ objects.h
@@ -2,7 +2,7 @@
 #define Objects_h
 
 #define PRODUCT_ID "esp7ina219"
-#define FIRMWARE_VERSION "1.2.2"
+#define FIRMWARE_VERSION "1.2.3"
 
 // Define the server and paths for OTA
 #define UPDATEURL "https://raw.githubusercontent.com/buildybee/beegreen-firmware-upgrade/refs/heads/main/esp7ina219.txt"
